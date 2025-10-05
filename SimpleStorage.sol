dce671c2bc17f067ecbec7d28b12e40a26373b66
Sheryl <jenishereyl@gmail.com>
2025-10-05 20:55:30 +0530

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract SimpleStorage{
+    uint myNumber;
+    string myName;
+
+    function setMyNumber(uint _num) public{
+        myNumber = _num;
+    }
+    function getMyNumber() public view returns(uint){
+        return myNumber;
+    }
+
+    function setMyName(string memory _name) public{
+        myName = _name; 
+    }
+     function getMyName() public view returns(string memory){
+        return myName;
+    }
+}
