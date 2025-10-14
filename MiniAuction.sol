486a610f0621699e2490d3b14261944554b323e3
sheryyll <jenishereyl@gmail.com>
2025-10-14 19:01:01 +0530

    Added MiniAuction.sol

---
+++ MiniAuction.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract MiniAuction{
+    address public owner;
+    uint public highestBid;
+    address public highestBidder;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    event NewBid( address  indexed bidder, uint amount);
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function bid() public payable{
+        require(msg.value > highestBid, "Bid must be higher than current highest");
+        highestBid = msg.value;
+        highestBidder = msg.sender;
+        emit NewBid(msg.sender, msg.value);
+    }
+
+    function endAuction() public onlyOwner{
+        require(highestBid > 0, "No bids placed yet");
+        payable(owner).transfer(highestBid);
+
+    }
+
+
+}
